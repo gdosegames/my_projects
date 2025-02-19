c5daa47241e3c0ac0af24e13a15ab83449c3dbbc
gdosegames <164591238+gdosegames@users.noreply.github.com>
2025-02-19 17:34:23 -0600

    Update 21_trump_cards.py

--- 21_trump_cards.py
+++ 21_trump_cards.py
@@ -5,12 +5,7 @@ cards3 = [1,2,3,4,5,6,7,8,9,10,11]
 cards4 = [1,2,3,4,5,6,7,8,9,10,11]
 onetwofor = [1,2,3,4]
 player_deck = []
-
-
-
-play = input("do you want to play?")
-
-while play:
+def pick_cardsplayer(player_deck,onetwofor,cards1,cards2,cards3,cards4):
     deck1 = random.choice(onetwofor)
     if (deck1 == 1):
         player_deck.append(random.choice(cards1))
@@ -31,10 +26,44 @@ while play:
         player_deck.append(random.choice(cards3))
     elif (deck2 == 4):
         player_deck.append(random.choice(cards4))
+    return
 
+def pick_cardscomputer(computer_deck,onetwofor,cards1,cards2,cards3,cards4):
+    deck1 = random.choice(onetwofor)
+    if (deck1 == 1):
+        computer_deck.append(random.choice(cards1))
+    elif (deck1 == 2):
+        computer_deck.append(random.choice(cards2))
+    elif (deck1 == 3):
+        computer_deck.append(random.choice(cards3))
+    elif (deck1 == 4):
+        computer_deck.append(random.choice(cards4))
+
+
+    deck2 = random.choice(onetwofor)
+    if (deck2 == 1):
+        computer_deck.append(random.choice(cards1))
+    elif (deck2 == 2):
+        computer_deck.append(random.choice(cards2))
+    elif (deck2 == 3):
+        computer_deck.append(random.choice(cards3))
+    elif (deck2 == 4):
+        computer_deck.append(random.choice(cards4))
+    return
+
+play = input("do you want to play?")
+
+while play:
+    pick_cardsplayer(player_deck,onetwofor,cards1,cards2,cards3,cards4)
+    pick_cardscomputer(computer_deck,onetwofor,cards1,cards2,cards3,cards4)
     print("youre cards are ")
     print(player_deck)
-
-
+    player_deck_sum = (player_deck(1) + player_deck(2))
+    if (player_deck_sum < 21):
+        draw1 = imput("do you want to draw?")
+            
+    elif (player_deck_sum = 21):
+        print("you have 21 you win!")
+    elif (player_deck_sum > 21):
 
     
