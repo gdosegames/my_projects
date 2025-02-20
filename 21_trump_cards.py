241394027b472175b1b1cd9a6996223a0bb52b93
gdosegames <164591238+gdosegames@users.noreply.github.com>
2025-02-19 18:58:48 -0600

    Update 21_trump_cards.py

--- 21_trump_cards.py
+++ 21_trump_cards.py
@@ -5,6 +5,10 @@ cards3 = [1,2,3,4,5,6,7,8,9,10,11]
 cards4 = [1,2,3,4,5,6,7,8,9,10,11]
 onetwofor = [1,2,3,4]
 player_deck = []
+computer_deck = []
+
+
+
 def pick_cardsplayer(player_deck,onetwofor,cards1,cards2,cards3,cards4):
     deck1 = random.choice(onetwofor)
     if (deck1 == 1):
@@ -58,12 +62,32 @@ while play:
     pick_cardscomputer(computer_deck,onetwofor,cards1,cards2,cards3,cards4)
     print("youre cards are ")
     print(player_deck)
-    player_deck_sum = (player_deck(1) + player_deck(2))
-    if (player_deck_sum < 21):
-        draw1 = imput("do you want to draw?")
-            
-    elif (player_deck_sum = 21):
-        print("you have 21 you win!")
-    elif (player_deck_sum > 21):
+    player_deck_sum = (player_deck[0] + player_deck[1])
+    print("\n")
+    playing = True
+    while playing:
 
+        if (player_deck_sum == 21):
+            print("you have 21 you win!")
+            break
+
+        if (player_deck_sum > 21):
+            print("you are over 21 game over")
+            break
+
+        if (player_deck_sum < 21):
+            draw1 = input("do you want to draw?")
+        if draw1 == True:
+            print("drawing")
+            deck1 = random.choice(onetwofor)
+            if (deck1 == 1):
+                player_deck.append(random.choice(cards1))
+            elif (deck1 == 2):
+                player_deck.append(random.choice(cards2))
+            elif (deck1 == 3):
+                player_deck.append(random.choice(cards3))
+            elif (deck1 == 4):
+                player_deck.append(random.choice(cards4))
+        elif draw1 == False:
+            if player_deck
     
