edafed3a46fda4073e26d17f0d0de8e0012d53a9
gdosegames <164591238+gdosegames@users.noreply.github.com>
2025-02-24 16:26:17 -0600

    Update 21_trump_cards.py

--- 21_trump_cards.py
+++ 21_trump_cards.py
@@ -7,82 +7,13 @@ onetwofor = [1,2,3,4]
 player_deck = []
 computer_deck = []
 
-
-def pick_cardsplayer(player_deck,onetwofor,cards1,cards2,cards3,cards4):
-    deck1 = random.choice(onetwofor)
-    if (deck1 == 1):
-        player_deck.append(random.choice(cards1))
-    elif (deck1 == 2):
-        player_deck.append(random.choice(cards2))
-    elif (deck1 == 3):
-        player_deck.append(random.choice(cards3))
-    elif (deck1 == 4):
-        player_deck.append(random.choice(cards4))
-
-
-    deck2 = random.choice(onetwofor)
-    if (deck2 == 1):
-        player_deck.append(random.choice(cards1))
-    elif (deck2 == 2):
-        player_deck.append(random.choice(cards2))
-    elif (deck2 == 3):
-        player_deck.append(random.choice(cards3))
-    elif (deck2 == 4):
-        player_deck.append(random.choice(cards4))
-    return
-
-def new_card_player(onetwofor,player_deck,cards1,cards2,cards3,cards4):
-    deck1 = random.choice(onetwofor)
-    if (deck1 == 1):
-        player_deck.append(random.choice(cards1))
-    elif (deck1 == 2):
-        player_deck.append(random.choice(cards2))
-    elif (deck1 == 3):
-        player_deck.append(random.choice(cards3))
-    elif (deck1 == 4):
-        player_deck.append(random.choice(cards4))
-
-def pick_cardscomputer(computer_deck,onetwofor,cards1,cards2,cards3,cards4):
-    deck1 = random.choice(onetwofor)
-    if (deck1 == 1):
-        computer_deck.append(random.choice(cards1))
-    elif (deck1 == 2):
-        computer_deck.append(random.choice(cards2))
-    elif (deck1 == 3):
-        computer_deck.append(random.choice(cards3))
-    elif (deck1 == 4):
-        computer_deck.append(random.choice(cards4))
-
-
-    deck2 = random.choice(onetwofor)
-    if (deck2 == 1):
-        computer_deck.append(random.choice(cards1))
-    elif (deck2 == 2):
-        computer_deck.append(random.choice(cards2))
-    elif (deck2 == 3):
-        computer_deck.append(random.choice(cards3))
-    elif (deck2 == 4):
-        computer_deck.append(random.choice(cards4))
-    return
-
-def new_card_computer(onetwofor,cards1,cards2,cards3,cards4,computer_deck):
-    deck1 = random.choice(onetwofor)
-    if (deck1 == 1):
-        computer_deck.append(random.choice(cards1))
-    elif (deck1 == 2):
-        computer_deck.append(random.choice(cards2))
-    elif (deck1 == 3):
-        computer_deck.append(random.choice(cards3))
-    elif (deck1 == 4):
-        computer_deck.append(random.choice(cards4))
-
-
+def give_card(cards1,cards2,cards3,cards4,onetwofor):
+    random.choice(onetwofor)
 
 play = input("do you want to play?")
 
 while play:
-    pick_cardsplayer(player_deck,onetwofor,cards1,cards2,cards3,cards4)
-    pick_cardscomputer(computer_deck,onetwofor,cards1,cards2,cards3,cards4)
+    player_deck.append
     print("youre cards are ")
     print(player_deck)
     player_deck_sum = (player_deck[0] + player_deck[1])
@@ -90,7 +21,7 @@ while play:
     print("\n")
     playing = True
     while playing:
-
+#player deck check 1
         if (player_deck_sum == 21):
             print("you have 21 you win!")
             break
@@ -104,6 +35,7 @@ while play:
             if (draw1 == True):
                 new_card_player(onetwofor,player_deck,cards1,cards2,cards3,cards4)
                 print(player_deck)
+#computer deck check 1
         if (computer_deck_sum == 21):
             print("the npc has 21 you lose!")
             break
@@ -114,7 +46,7 @@ while play:
             
         if (computer_deck_sum < 17):
             new_card_computer(onetwofor,cards1,cards2,cards3,cards4,computer_deck)
-
+#player deck check 2
         print(player_deck)
         if (player_deck_sum == 21):
             print("you have 21 you win!")
@@ -126,5 +58,9 @@ while play:
 
         if (player_deck_sum < 21):
             draw1 = input("do you want to draw?")
-        
+            if (draw1 == True):
+                new_card_player(onetwofor,player_deck,cards1,cards2,cards3,cards4)
+                print(player_deck)
+
+
 
