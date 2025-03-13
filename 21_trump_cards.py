bbaac8379282eb24173a70b55fc946b13ef2cc63
gdosegames <164591238+gdosegames@users.noreply.github.com>
2025-03-12 20:48:33 -0500

    Update 21_trump_cards.py

--- 21_trump_cards.py
+++ 21_trump_cards.py
@@ -12,6 +12,7 @@ random.shuffle(cards1)
 random.shuffle(cards2)
 random.shuffle(cards3)
 random.shuffle(cards4)
+#-----------------------#
 #gives new card
 def give_card():
     global onetwofor, cards1, cards2, cards3, cards4
@@ -31,36 +32,6 @@ def count_cards(cards_in_deck):
         sum += cards_in_deck[i]
     return sum
 
-def draw_or_stay():
-    global player_deck_sum
-    if (check_sum()):
-            print("player 1")
-            print("1 to draw")
-            print("2 to skip")
-            draw1 = int(input("what is you're choice?"))
-            #draw new card player
-            if (draw1 == 1):
-                player_deck.append(give_card())
-                print(player_deck)
-                player_deck_sum = count_cards(player_deck)
-                print("you have", player_deck_sum)
-
-def draw_or_stay2():
-    global computer_deck_sum
-    if (check_sum()):
-        print("player 2")
-        print("1 to draw")
-        print("2 to skip")
-        draw2 = int(input("what is you're choice?"))
-        #draw new card player
-        if (draw2 == 1):
-            computer_deck.append(give_card())
-            print(computer_deck)
-            computer_deck_sum = count_cards(computer_deck)
-            print("you have", computer_deck_sum)
-    
-
-
 print("21 by")
 print("garrett")
 print("ayden")
@@ -71,7 +42,7 @@ print("you're options are")
 print("1 to play")
 print("2 to quit")
 play = int(input("what is you're choice? "))
-
+print("\n")
 if (play == 1):
     player_deck.append(give_card())
     player_deck.append(give_card())
@@ -79,48 +50,74 @@ if (play == 1):
     computer_deck.append(give_card())
     computer_deck.append(give_card())
 
-    print("youre cards are ")
-    print(player_deck)
     computer_deck_sum = (computer_deck[0] + computer_deck[1])
     player_deck_sum = (player_deck[0] + player_deck[1])
-    print("you have", player_deck_sum)
-
+    print("player 1 cards are ")
+    print(player_deck)
+    print("player 1 has", player_deck_sum)
+    print("\n")
+    print("player 2 cards are ")
+    print(computer_deck)
+    print("player 2 has", computer_deck_sum)
+    print("\n")
+    
     playing = True
     while playing:
     #player deck check 1
-        draw_or_stay()
+        if (player_deck_sum < 21):
+            print("player 1")
+            print("1 to draw")
+            print("2 to skip")
+            draw1 = int(input("what is you're choice?"))
+            #draw new card player
+            if (draw1 == 1):
+                player_deck.append(give_card())
+                print(player_deck)
+                player_deck_sum = count_cards(player_deck)
+                print("player 1 has", player_deck_sum)
+            print("\n")    
 
         if (player_deck_sum == 21):
-            print("you have 21 you win!")
+            print("player 1 has 21 they win!")
             break
 
         if (player_deck_sum > 21):
-            print("you are over 21 game over")
+            print("player 1 is over 21 they lose")
             break
     #computer deck check 1
         if (computer_deck_sum == 21):
-            print("the npc has 21 you lose!")
+            print("player 2 has 21 they win!")
             break
 
         if (computer_deck_sum > 21):
-            print("the npc is over 21 you win")
+            print("player 2 is over 21 they lose")
             break
         #draw new card computer
         
-        draw_or_stay2()
-       
-        
-        print("you have", player_deck_sum)
-        print("the computer has", computer_deck_sum)
-        if (computer_deck_sum > player_deck_sum):
-            print("you lose ")
-            break
-        elif (computer_deck_sum < player_deck_sum):
-            print("you win ")
-            break
-        elif (computer_deck_sum == player_deck_sum):
-            print("tie ")
-            break
+        if (player_deck_sum < 21):
+            print("player 2")
+            print("1 to draw")
+            print("2 to skip")
+            draw2 = int(input("what is you're choice?"))
+            #draw new card player
+            if (draw2 == 1):
+                computer_deck.append(give_card())
+                print(computer_deck)
+                computer_deck_sum = count_cards(computer_deck)
+                print("player 2 has", computer_deck_sum)        
+            print("\n")        
+        if (draw1 == 2) and (draw2 == 2):
+            print("player 1 has", player_deck_sum)
+            print("player 2 has", computer_deck_sum)
+            if (computer_deck_sum > player_deck_sum):
+                print("player 2 wins ")
+                break
+            elif (computer_deck_sum < player_deck_sum):
+                print("player 1 wins ")
+                break
+            elif (computer_deck_sum == player_deck_sum):
+                print("tie ")
+                break
 
 elif(play == 2):
     print("ok by")  
@@ -130,4 +127,3 @@ else:
 # print(f"DEBUG: Deck 1: {cards1}\nDEBUG: Deck 2: {cards2}\nDEBUG: Deck 3: {cards3}\nDEBUG: Deck 4: {cards4}")
 # print(give_card())
 # print(f"DEBUG: Deck 1: {cards1}\nDEBUG: Deck 2: {cards2}\nDEBUG: Deck 3: {cards3}\nDEBUG: Deck 4: {cards4}")
-
